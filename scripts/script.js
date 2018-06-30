46f868e63cb66dc8d755eb035f0cc3901aaa4267
Calstream <calstream.lgn@gmail.com>
2018-06-30 21:58:23 +0300

    added drawing on hover

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,8 @@
+let penColor = "#000000";
+let randomColor = false;
+let gradual = true;
+let intensity = 0.1; //percent;
+
 function getRandomColor() {
   var letters = '0123456789ABCDEF';
   var color = '#';
@@ -7,7 +12,62 @@ function getRandomColor() {
   return color;
 }
 
-//let cellSize = prompt("square size?");
+function int_to_hex(num)
+{
+    var hex = Math.round(num).toString(16);
+    if (hex.length == 1)
+        hex = '0' + hex;
+    return hex;
+}
+
+function blendColors(color1, color2, percentage)
+{
+    if (color2.length == 4)
+        color2 = color2[1] + color2[1] +
+        color2[2] + color2[2] +
+        color2[3] + color2[3];
+    else
+        color2 = color2.substring(1);
+
+    console.log('valid: c1 => ' + color1 + ', c2 => ' + color2);
+    color1 = color1.split("(")[1].split(")")[0].split(",");
+    color2 = [parseInt(color2[0] + color2[1], 16), parseInt(color2[2] + color2[3], 16), parseInt(color2[4] + color2[5], 16)];
+
+    console.log('hex -> rgba: c1 => [' + color1.join(', ') + '], c2 => [' + color2.join(', ') + ']');
+
+    // 4: blend
+    var color3 = [
+        (1 - percentage) * color1[0] + percentage * color2[0],
+        (1 - percentage) * color1[1] + percentage * color2[1],
+        (1 - percentage) * color1[2] + percentage * color2[2]
+    ];
+
+    console.log('c3 => [' + color3.join(', ') + ']');
+
+    // 5: convert to hex
+    color3 = '#' + int_to_hex(color3[0]) + int_to_hex(color3[1]) + int_to_hex(color3[2]);
+
+    console.log("result: " + color3);
+
+    return color3;
+}
+
+function getCellColor(div)
+{
+  if (gradual)
+  {
+    let cellColor = div.style.backgroundColor;
+    if (!randomColor)
+      return blendColors(cellColor, penColor, intensity);
+    else
+      return blendColors(cellColor, getRandomColor(), intensity);
+  }
+  else if (randomColor)
+    return getRandomColor();
+  else
+    return penColor;
+}
+
 let grid = document.querySelector("#grid");
 grid.style.backgroundColor = "#fff";
 let gridHeight = grid.offsetHeight;
@@ -19,7 +79,7 @@ let i = 0;
 let max = gridWidth * gridHeight / (cellSize ** 2);
 console.log(gridWidth + " " + gridHeight + " " + max);
 
-for (i = 0; i < max; i++)
+for (i = 0; i < 608; i++)
   {
     //grid.style.width = 3*cellSize + "px";
     //grid.style.height = 3*cellSize + "px";
@@ -28,6 +88,7 @@ for (i = 0; i < max; i++)
     cell.style.backgroundColor = getRandomColor();
     cell.style.width = cellSize + "px";
     cell.style.height = cellSize + "px";
+    cell.onmouseover = function() {
+      this.style.backgroundColor = getCellColor(this); }
     grid.appendChild(cell);
-    console.log(`${i}`);
   }
